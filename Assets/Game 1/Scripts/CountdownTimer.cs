5c56cb871dfa8bb61fa34e106969fb63fda031a2
Elejarken16 <72119579+Elejarken16@users.noreply.github.com>
2021-09-24 15:41:10 +1000

    sweet
    .

---
+++ Assets/Game 1/Scripts/CountdownTimer.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+
+public class CountdownTimer : MonoBehaviour
+{
+    int countDownStartValue = 60;
+    public float totalTime; 
+    public Text timerUI;
+    
+
+    void Start()
+    {
+        countDownTimer();
+    }
+
+    void countDownTimer()
+    {
+        if(countDownStartValue > 0)
+        {
+            TimeSpan spanTime = TimeSpan.FromSeconds(countDownStartValue);
+            timerUI.text = "" + spanTime.Minutes + " : " + spanTime.Seconds;
+            countDownStartValue--;
+            Invoke("countDownTimer", 1.0f);
+        }
+        else
+        {
+            timerUI.text = "GameOver!";
+            SceneManager.LoadScene(2);
+        }
+    }
+
+    
+}
