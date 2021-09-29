32046992ed006327e3af564b2aa664012f82d0ee
Elejarken16 <72119579+Elejarken16@users.noreply.github.com>
2021-09-29 20:57:45 +1000

    level 5 done

--- Assets/Game 1/Scripts/Week1LoadLvl5.cs
+++ Assets/Game 1/Scripts/Week1LoadLvl5.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class LoadLvl5 : MonoBehaviour
+public class Week1LoadLvl5 : MonoBehaviour
 {
     private void OnTriggerEnter(Collider other)
     {
